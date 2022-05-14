7006ab92c128f99d21a63a7a080010f3927f40f8
Benie <davinci.monalissa@gmail.com>
2022-05-14 13:39:22 +0300

    add a logout view function.

--- app/auth/views.py
+++ app/auth/views.py
@@ -39,3 +39,10 @@ def login():
     title = 'ThyVoice Login'
 
     return render_template('auth/login.html',login_form=login_form,title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
