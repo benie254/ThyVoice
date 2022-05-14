f18044454e08c8fc4b66a1ba7d7512e82729fe03
Benie <davinci.monalissa@gmail.com>
2022-05-14 13:18:44 +0300

    create a registration form.

---
+++ app/auth/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import InputRequired,Email,EqualTo
+from wtforms import ValidationError
+from ..models import Users
+
+
+class RegistrationForm(FlaskForm):
+    """
+    registers new users
+    """
+
+    email = StringField('Your email address...',validators=[InputRequired(),Email()])
+    username = StringField('Your username...',validators=[InputRequired()])
+    password = PasswordField('Your password...', validators=[InputRequired(),EqualTo('password_confirm',message='Passwords must match')])
+    password_confirm = PasswordField('Confirm your password',validators=[InputRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        """
+        :param data_field: email data
+        :return: validated address--checks if an account exists
+        """
+
+        if Users.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        """
+        :param data_field: username data
+        :return: validated user--checks if username exists
+        """
+        if Users.query.filter_by(username=data_field.data).first():
+            raise ValidationError('That username exists')
