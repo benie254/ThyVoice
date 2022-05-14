8c276e5c557f34879e3428bf34919aa30f011eeb
Benie <davinci.monalissa@gmail.com>
2022-05-14 13:06:09 +0300

    add a manager.shell decorator--to make shell context.

--- manage.py
+++ manage.py
@@ -22,3 +22,8 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, Users=Users)
