f1ced233e1170d10237f483e8de74f58374c02e6
Benie <davinci.monalissa@gmail.com>
2022-05-14 13:38:52 +0300

    add & update login view function && update signup view function.

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,10 +16,26 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to thePitcher","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to ThyVoice","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
 
     title = 'New Account'
 
     return render_template('auth/register.html',form=form,title=title)
+
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = Users.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or password')
+
+    title = 'ThyVoice Login'
+
+    return render_template('auth/login.html',login_form=login_form,title=title)
