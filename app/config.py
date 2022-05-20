aaa0b6daaaaae9efb6fafcb8ba7df7e2f1f9d532
Benie <davinci.monalissa@gmail.com>
2022-05-20 18:53:16 +0300

    deployment to heroku

--- app/config.py
+++ app/config.py
@@ -31,7 +31,9 @@ class ProdConfig(Config):
 
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://benie:12345@localhost/thyvoice'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://benie:12345@localhost/thyvoice'
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     DEBUG = True
 
