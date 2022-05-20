c46902b1758ff00eeb4ed0ffb1268dabb5f17909
Benie <davinci.monalissa@gmail.com>
2022-05-20 20:37:07 +0300

    fix errors

--- app/config.py
+++ app/config.py
@@ -45,9 +45,9 @@ class DevConfig(Config):
 
     DEBUG = True
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://benie:12345@localhost/thyvoice'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://benie:12345@localhost/thyvoice'
 
 
 config_options = {
