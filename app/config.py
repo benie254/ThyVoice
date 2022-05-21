a23580af6656fc9592c94ca513a3d018108a146b
Benie <davinci.monalissa@gmail.com>
2022-05-21 05:04:18 +0300

    another deployment trial

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://benie:12345@localhost/thyvoice'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://benie:12345@localhost:5500/thyvoice'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
